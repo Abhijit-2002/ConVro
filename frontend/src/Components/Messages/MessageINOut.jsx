4e6e78ccb8c9302ca4ae0bff8995d0248588eed6
Abhijit Chakraborty <115333386+Abhijit-2002@users.noreply.github.com>
2024-10-30 02:25:15 +0530

    changes

--- frontend/src/Components/Messages/MessageINOut.jsx
+++ frontend/src/Components/Messages/MessageINOut.jsx
@@ -1,39 +1,43 @@
-import React from 'react'
-import {useAuthContext} from '../../context/AuthContext.jsx'
-import { extractTime } from "../../utils/extractTime.js";
+import React from "react";
+import { useAuthContext } from "../../context/AuthContext.jsx";
+//import { extractTime } from "../../utils/extractTime";
 import useConversation from "../../zustand/useConversation";
 
-
-
-const MessageINOut = ({message}) => {
-  const {authUser} =useAuthContext();
+const MessageINOut = ({ message }) => {
+  const { authUser } = useAuthContext();
   const { selectedConversation } = useConversation();
-	const fromMe = message.senderId === authUser._id;
-	const formattedTime = extractTime(message.createdAt);
-	const chatClassName = fromMe ? "chat-end" : "chat-start";
-	const profilePic = fromMe ? authUser.profilepic : selectedConversation?.profilepic;
-	const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+  const fromMe = message.senderId === authUser._id;
+  //const formattedTime = extractTime(message.createdAt);
+  const formattedTime = message.createdAt
+    ? new Date(message.createdAt).toLocaleTimeString()
+    : "Unknown time";
+  const chatClassName = fromMe ? "chat-end" : "chat-start";
+  const profilePic = fromMe
+    ? authUser.profilepic
+    : selectedConversation?.profilepic;
+  const bubbleBgColor = fromMe ? "bg-blue-500" : "";
 
-
-	const shakeClass = message.shouldShake ? "shake" : "";
+  const shakeClass = message.shouldShake ? "shake" : "";
   return (
-
     <div className={`chat ${chatClassName}`}>
-        <div className='chat-image avatar'>
-            <div className='w-10 rounded-full'>
-                <img src={profilePic} alt="User avatar" />
-            </div>
+      <div className="chat-image avatar">
+        <div className="w-10 rounded-full">
+          <img src={profilePic} alt="User avatar" />
         </div>
-        <div className={`chat-bubble tetx-white   ${bubbleBgColor} ${shakeClass} pb-2`}>{message.message}</div>
-        <div className=' chat-footer opacity-50 text-xs flex gap-1 items-center '>{formattedTime}</div> 
+      </div>
+      <div
+        className={`chat-bubble tetx-white   ${bubbleBgColor} ${shakeClass} pb-2`}
+      >
+        {message.message}
+      </div>
+      <div className=" chat-footer opacity-50 text-xs flex gap-1 items-center ">
+        {formattedTime}
+      </div>
     </div>
-   
-    
-  )
-}
+  );
+};
 
-export default MessageINOut
-
+export default MessageINOut;
 
 // import React from 'react'
 
@@ -49,8 +53,7 @@ export default MessageINOut
 //         <div className='chat-bubble tetx-white bg-blue-500'>Hi! Whats Up?</div>
 //         <div className=' chat-footer opacity-50 text-xs flex gap-1 items-center '>12:42</div>
 //     </div>
-   
-    
+
 //   )
 // }
 
--- frontend/src/hooks/useGetMessage.js
+++ frontend/src/hooks/useGetMessage.js
@@ -4,27 +4,28 @@ import toast from "react-hot-toast";
 
 const useGetMessage = () => {
   const [loading, setLoading] = useState(false);
-	const { messages, setMessages, selectedConversation } = useConversation();
+  const { messages, setMessages, selectedConversation } = useConversation();
 
-	useEffect(() => {
-		const getMessages = async () => {
-			setLoading(true);
-			try {
-				const res = await fetch(`/api/messages/${selectedConversation._id}`);
-				const data = await res.json();
-				if (data.error) throw new Error(data.error);
-				setMessages(data);
-			} catch (error) {
-				toast.error(error.message);
-			} finally {
-				setLoading(false);
-			}
-		};
+  useEffect(() => {
+    const getMessages = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch(`/api/messages/${selectedConversation._id}`);
+        const data = await res.json();
+        if (data.error) throw new Error(data.error);
+        setMessages(data);
+      } catch (error) {
+        toast.error(error.message);
+      } finally {
+        setLoading(false);
+		
+      }
+    };
 
-		if (selectedConversation?._id) getMessages();
-	}, [selectedConversation?._id, setMessages]);
+    if (selectedConversation?._id) getMessages();
+  }, [selectedConversation?._id, setMessages]);
 
-	return { messages, loading };
+  return { messages, loading };
 };
 
 export default useGetMessage;
