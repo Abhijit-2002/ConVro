f114567c06e8564631e3e455f22af047fe0dcf2e
Abhijit Chakraborty <115333386+Abhijit-2002@users.noreply.github.com>
2024-10-30 00:14:05 +0530

    "Removed extra space in class names in MessageINOut component"

--- frontend/src/Components/Messages/MessageINOut.jsx
+++ frontend/src/Components/Messages/MessageINOut.jsx
@@ -24,8 +24,8 @@ const MessageINOut = ({message}) => {
                 <img src={profilePic} alt="User avatar" />
             </div>
         </div>
-        <div className={`chat-bubble tetx-white  ${bubbleBgColor} ${shakeClass} pb-2`}>{message.message}</div>
-        <div className=' chat-footer opacity-50 text-xs flex gap-1 items-center '>{formattedTime}</div>
+        <div className={`chat-bubble tetx-white   ${bubbleBgColor} ${shakeClass} pb-2`}>{message.message}</div>
+        <div className=' chat-footer opacity-50 text-xs flex gap-1 items-center '>{formattedTime}</div> 
     </div>
    
     
